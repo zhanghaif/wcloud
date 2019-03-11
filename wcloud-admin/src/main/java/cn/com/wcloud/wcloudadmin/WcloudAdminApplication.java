126a3012132b899eb85b9a80406e6bb2e35defee
张海峰 <zhanghaifeng@zhanghaifengdeMacBook-Pro-2.local>
2019-03-11 19:18:19 +0800

    admin

---
+++ wcloud-admin/src/main/java/cn/com/wcloud/wcloudadmin/WcloudAdminApplication.java
@@ -0,0 +1,13 @@
+package cn.com.wcloud.wcloudadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WcloudAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WcloudAdminApplication.class, args);
+	}
+
+}
