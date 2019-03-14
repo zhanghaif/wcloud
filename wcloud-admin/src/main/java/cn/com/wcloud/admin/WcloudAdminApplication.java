cb851f4a0fb8a417a4ba471ef05c3ad721bc52eb
张海峰 <zhanghaifeng@zhanghaifengdeMacBook-Pro-2.local>
2019-03-14 10:19:55 +0800

    更改结构

--- wcloud-admin/src/main/java/cn/com/wcloud/admin/WcloudAdminApplication.java
+++ wcloud-admin/src/main/java/cn/com/wcloud/admin/WcloudAdminApplication.java
@@ -1,4 +1,4 @@
-package cn.com.wcloud.wcloudadmin;
+package cn.com.wcloud.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
